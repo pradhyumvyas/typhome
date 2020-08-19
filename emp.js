10254a65594b3a588a7e0d6f7b30d903cf4ad49f
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-08-20 03:10:38 +0530

    Added feature of avoid duplicasy

--- emp.js
+++ emp.js
@@ -1,20 +1,23 @@
-var arr1 = [];
+var arr1 = new Array();
+
 
 function addItem() {
   var lists = document.getElementById("listItems");
   var inputdata = "👉 " + document.getElementById("inputdata").value;
   var li = document.createElement("li");
 
-  //   alert("'jfhjd");
-  arr1.push(inputdata);
-  console.log(arr1);
   if (inputdata === "") {
     alert("Enter the Text First");
   } else {
-    li.setAttribute("id", inputdata);
-    li.appendChild(document.createTextNode(inputdata));
-    lists.appendChild(li);
-    arr1.push(inputdata);
-    // console.log(arr1);
+
+    if (arr1.includes(inputdata)){
+      alert("News are already there")
+    } 
+    else{
+      li.setAttribute("id", inputdata);
+      li.appendChild(document.createTextNode(inputdata));
+      lists.appendChild(li);
+      arr1.push(inputdata);
+    }
   }
 }
