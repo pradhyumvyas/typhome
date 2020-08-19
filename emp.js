eb31dbd3a0d01941fdd019d95d7c61e5f75d38c0
harshdeepy205 <harshdeep205@outlook.com>
2020-08-20 02:24:04 +0530

    third

--- emp.js
+++ emp.js
@@ -1,9 +1,20 @@
+var arr1 = [];
+
 function addItem() {
   var lists = document.getElementById("listItems");
-  var inputdata = document.getElementById("inputdata");
+  var inputdata = "👉 " + document.getElementById("inputdata").value;
   var li = document.createElement("li");
 
-  li.setAttribute("id", inputdata.value);
-  li.appendChild(document.createTextNode(inputdata.value));
-  lists.appendChild(li);
+  //   alert("'jfhjd");
+  arr1.push(inputdata);
+  console.log(arr1);
+  if (inputdata === "") {
+    alert("Enter the Text First");
+  } else {
+    li.setAttribute("id", inputdata);
+    li.appendChild(document.createTextNode(inputdata));
+    lists.appendChild(li);
+    arr1.push(inputdata);
+    // console.log(arr1);
+  }
 }
